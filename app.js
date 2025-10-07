e722f5f0ab314c62c8bbafa4f980cc92d869155e
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-07 04:08:28 +0200

    add cookies

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load .env variables
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser()); // must come before routes
 app.set('view engine', 'ejs');
+
+
+
 
 // Build MongoDB connection string from environment variables
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}`;
@@ -28,3 +34,24 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
